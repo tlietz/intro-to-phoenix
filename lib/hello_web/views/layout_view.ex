9dee8188224f9847219f09242bb2608c7ea31919
tlietz <theodore.lietz@gmail.com>
2022-04-03 15:51:12 -0500

    finish views and templates

--- lib/hello_web/views/layout_view.ex
+++ lib/hello_web/views/layout_view.ex
@@ -1,5 +1,11 @@
+# Functions defined here can be called in the respective template,
+# because all templates are effectively compiled into functions inside their respective view
 defmodule HelloWeb.LayoutView do
   use HelloWeb, :view
+
+  def title() do
+    "Awesome title!"
+  end
 
   # Phoenix LiveDashboard is available only in development by default,
   # so we instruct Elixir to not warn if the dashboard route is missing.
